bd088f7996c1a5d7b8f4bef803f1859af1e3cbbc
Amit kumar <amitk.kumar414@gmail.com>
2024-07-16 16:37:47 +0530

    first commit

---
+++ testimonial-slider/script.js
@@ -0,0 +1,43 @@
+const image = document.getElementById("image");
+const testimonial = document.getElementById("testimonial");
+const userName = document.getElementById("name");
+
+const testimonials = [
+  {
+    name: "Cherise G",
+    photoUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
+  },
+  {
+    name: "Rosetta Q",
+    photoUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
+  },
+  {
+    name: "Constantine V",
+    photoUrl:
+      "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
+  },
+];
+
+let i = 0;
+
+update();
+
+function update() {
+  const { name, photoUrl, text } = testimonials[i];
+  image.src = photoUrl;
+  testimonial.innerText = text;
+  userName.innerText = name;
+  i++;
+  if (i === testimonials.length) {
+    i = 0;
+  }
+
+  setTimeout(() => {
+    update();
+  }, 5000);
+}
