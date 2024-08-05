7466fd6ce74391afb86a7776f36f044723821049
Amit kumar <amitk.kumar414@gmail.com>
2024-08-05 12:11:24 +0530

    word and letter counter added

---
+++ word-counter/script.js
@@ -0,0 +1,20 @@
+const textarea = document.getElementById("text-area");
+const wordcount = document.getElementById("word-count");
+const lettercount = document.getElementById("letter-count");
+
+textarea.addEventListener("keyup", () => {
+  count();
+});
+
+const count = () => {
+  lettercount.innerText = textarea.value.length;
+  let letter = textarea.value;
+  word = letter.trim().split(" ");
+  let filtered = word.filter(function (el) {
+    return el != null || " ";
+  });
+  console.log(filtered);
+
+    wordcount.innerText = filtered.length;
+
+};
